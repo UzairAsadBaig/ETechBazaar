959db9c0378833b6d0de323b9261b376b17ac2f6
Muzamil-07 <hafizmuzammiljo9@gmail.com>
2021-09-16 19:42:09 +0500

    minor changes

--- Public/js/script.js
+++ Public/js/script.js
@@ -3,26 +3,10 @@
 //-----------------------------------------------DOM Elements
 const reduceProd = document.querySelector(".product_reduce");
 const increaseProd = document.querySelector(".product_increase");
-const quantityVal = document.querySelector(".quantity_val");
-const buttons = document.querySelector(".buttons");
-const adminBody = document.querySelectorAll(".tabbed-body");
+const quantityVal = document.querySelector( ".quantity_val" );
 
-//Tabbed Containers
-buttons.addEventListener("click", function (event) {
-  event.preventDefault();
-  const btn = event.target;
-  if ( btn.classList.contains( "btns" ) ) {
-    Array.from(btn.parentElement.children).forEach((element) =>
-      element.classList.remove("activeBtn")
-    );
-    btn.classList.add("activeBtn");
-    adminBody.forEach((i) => i.classList.remove("activeTab"));
-    document
-      .querySelector(`.tab${btn.getAttribute("num")}`)
-      .classList.add("activeTab");
-    console.log(adminBody);
-  }
-});
+
+
 
 //Order products quntity
 let quantity = Number(quantityVal.innerHTML);
