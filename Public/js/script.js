217db1e34e8513d906a705054ddb95e8189d8545
Muhammad Uzair Baig <bsef19m540@pucit.edu.pk>
2021-09-16 17:09:14 +0500

    Added the admin panel

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,19 +1,40 @@
 "use strict";
-const reduceProd = document.querySelector( '.product_reduce' );
-const increaseProd = document.querySelector( '.product_increase' );
-const quantityVal = document.querySelector( '.quantity_val' );
 
-let quantity = Number( quantityVal.innerHTML );
+//-----------------------------------------------DOM Elements
+const reduceProd = document.querySelector(".product_reduce");
+const increaseProd = document.querySelector(".product_increase");
+const quantityVal = document.querySelector(".quantity_val");
+const buttons = document.querySelector(".buttons");
+const adminBody = document.querySelectorAll(".tabbed-body");
 
-increaseProd.addEventListener( 'click', function () {
-    quantityVal.innerHTML = quantity + 1;
-    quantity++;
-} )
+//Tabbed Containers
+buttons.addEventListener("click", function (event) {
+  event.preventDefault();
+  const btn = event.target;
+  if (btn.classList.contains("btns")) {
+    Array.from(btn.parentElement.children).forEach((element) =>
+      element.classList.remove("activeBtn")
+    );
+    btn.classList.add("activeBtn");
+    adminBody.forEach((i) => i.classList.remove("activeTab"));
+    document
+      .querySelector(`.tab${btn.getAttribute("num")}`)
+      .classList.add("activeTab");
+    console.log(adminBody);
+  }
+});
 
-reduceProd.addEventListener( 'click', function () {
-    if ( quantity !== 1 ) {
-        quantityVal.innerHTML = quantity - 1;
-        quantity--;
-    }
+//Order products quntity
+let quantity = Number(quantityVal.innerHTML);
 
-} )
+increaseProd.addEventListener("click", function () {
+  quantityVal.innerHTML = quantity + 1;
+  quantity++;
+});
+
+reduceProd.addEventListener("click", function () {
+  if (quantity !== 1) {
+    quantityVal.innerHTML = quantity - 1;
+    quantity--;
+  }
+});
