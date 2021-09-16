749fa632ed7634ea02e1aa23283eb9e2db174416
Muhammad Uzair Baig <66635170+UzairAsadBaig@users.noreply.github.com>
2021-09-16 20:37:36 +0500

    Admin portal added

---
+++ Public/js/admin.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//-----------------------------------------------DOM Elements
+const buttons = document.querySelector(".buttons");
+const adminBody = document.querySelectorAll(".tabbed-body");
+
+//Tabbed Containers
+buttons.addEventListener("click", function (event) {
+  event.preventDefault();
+  const btn = event.target;
+  if (btn.classList.contains("btns")) {
+    Array.from(btn.parentElement.children).forEach((element) =>
+      element.classList.remove("activeBtn")
+    );
+    btn.classList.add("activeBtn");
+    adminBody.forEach((i) => i.classList.remove("activeTab"));
+    document
+      .querySelector(`.tab${btn.getAttribute("num")}`)
+      .classList.add("activeTab");
+  }
+});
