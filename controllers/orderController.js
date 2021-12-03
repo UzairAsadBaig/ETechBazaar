d6e13cc1d5f2e6bf6ee5b03e8efbd00223ec5d94
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-12-03 18:32:16 +0500

    single order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 const Order = require("../models/orderModel");
 const catchAsync = require("../utils/catchAsync");
-
+//all orders
 exports.getAllOrders = catchAsync(async (req, res, next) => {
   const orders = await Order.find();
 
@@ -9,7 +9,7 @@ exports.getAllOrders = catchAsync(async (req, res, next) => {
   }
   res.status(200).json(orders);
 });
-
+//new order
 exports.newOrder = catchAsync(async (req, res, next) => {
   // res.send(req.body);
   const order = new Order(req.body);
@@ -18,5 +18,19 @@ exports.newOrder = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: "success",
     data: order,
+  });
+});
+//single order
+exports.getOrder = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  const ord = await Order.findById(id);
+
+  if (!ord) {
+    return next(new AppError("No Product Found!", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    product: ord,
   });
 });
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 //  '/' order
 router.get("/", orderController.getAllOrders); // get all orders
 router.post("/", orderController.newOrder); //post new order
+router.get("/:id", orderController.getOrder); // get one order based on id
 
 module.exports = router;
