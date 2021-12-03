3ce98c782f50d81764671dd877066ca80508d263
Muhammad Uzair Baig <66635170+UzairAsadBaig@users.noreply.github.com>
2021-12-03 16:35:37 +0500

    Navbar
    Made the final version of navbar design with additional features

---
+++ Public/js/hideNav.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 25) {
+    //Here 200 may be not be exactly 200px
+    if ($(window).width() < 576) $(".fixedelement").hide();
+  } else {
+    //Here 200 may be not be exactly 200px
+    if ($(window).width() < 576) $(".fixedelement").show();
+  }
+});
