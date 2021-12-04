05aa83747f291199676030c0c5d3e8be73934bf5
Muhammad Uzair Baig <66635170+UzairAsadBaig@users.noreply.github.com>
2021-12-04 18:27:38 +0500

    Added overview

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use("/", viewRouter);
+// app.use("/", viewRouter);
 
 //Providing a static path
 app.use(express.static(path.join(__dirname, "public")));
